82ed6727eb12dea1d8efbe340248d21fc3dffd7b
OSSahota <osstechunlimited@gmail.com>
2018-12-31 12:38:36 +0000

    Completed POST/redirect/GET pattern

--- app.rb
+++ app.rb
@@ -1,23 +1,36 @@
 require 'sinatra/base'
 
 class Battle < Sinatra::Base
+  enable :sessions
 
-  # app code goes here...
-
-  # Only required during first few setup challenges to test the app was running correctly.
   get '/' do
-    # 'Hello Battle!'
-    # 'Testing infrastructure working!'
-
     # US#1-Entering players ch
     erb(:index)
   end
 
   post '/names' do
     p params
-    @player_1_name = params[:player_1_name]
-    @player_2_name = params[:player_2_name]
+    # Entering players ch.
+    # @player_1_name = params[:player_1_name]
+    # @player_2_name = params[:player_2_name]
+    # Experimenting - this is one way of returning expected content output
+    # but not good practice, should render a view e.g. erb(:play).
     # "#{@player_1_name} vs. #{@player_2_name}"
+
+    # Refactoring US#1 via PrG design pattern.
+    # Storing name values held in params hash to the session.
+    session[:player_1_name] = params[:player_1_name]
+    session[:player_2_name] = params[:player_2_name]
+    # Redirecting rendering of view to get '/play' route.
+    # erb(:play)
+    redirect '/play'
+  end
+
+  # Route created as part of refactoring US#1 via PrG design pattern.
+  get '/play' do
+    # Extracting values from session to instance variables.
+    @player_1_name = session[:player_1_name]
+    @player_2_name = session[:player_2_name]
     erb(:play)
   end
 
