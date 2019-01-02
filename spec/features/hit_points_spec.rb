c36a108cc46ed1ce686ae2bb30e6adb370b79c0b
OSSahota <osstechunlimited@gmail.com>
2019-01-02 16:45:18 +0000

    Completed Multiplayer:User story 6 - player 1 to see their own hit points

--- spec/features/hit_points_spec.rb
+++ spec/features/hit_points_spec.rb
@@ -1,7 +1,13 @@
 feature 'View hit points' do
   scenario 'Viewing player 2 hit points' do
-    p '*****hit_points_spec*****'
+    p '*****hit_points_spec-viewing player 2 hit points*****'
     sign_in_and_play
     expect(page).to have_content "Mittens: 60HP"
+  end
+
+  scenario 'Viewing player 1 hit points' do
+    p '*****hit_points_spec-viewing player 1 hit points*****'
+    sign_in_and_play
+    expect(page).to have_content "Dave: 60HP"
   end
 end
